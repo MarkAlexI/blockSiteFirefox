08c6c4c09ca89cf8bc9bd2bdffe2b963a8148785
MarkAlexI <markovaleksandr108@gmail.com>
2025-08-01 22:40:06 +0300

    fix(tab-close): prevent error when attempting to close a tab not created by script. second attempt

--- scripts/blocked.js
+++ scripts/blocked.js
@@ -1,12 +1,7 @@
 const closeBtn = document.getElementById('closeBtn');
 
 closeBtn.addEventListener('click', () => {
-  try {
-    window.close();
-  }
-  catch (e) {
-    browser.runtime.sendMessage({
-      type: 'close_current_tab'
-    });
-  }
+  browser.runtime.sendMessage({
+   type: 'close_current_tab'
+  });
 });
