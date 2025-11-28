46f87dd988af0e5b4010047c33382d1efc96e7f3
MarkAlexI <markovaleksandr108@gmail.com>
2025-11-28 17:22:51 +0200

    chore(i18n): fix several translation issues

--- update/update.js
+++ update/update.js
@@ -9,7 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
     'Added password protection for rule deletion in Pro mode',
     'Improved the statistics system — it now tracks not only site blocks but also redirects.',
     'This helps make analytics more complete and accurate, so future improvements can be even smarter.', */
-    'This update improves how rule changes take effect. Now, whenever you add or edit a blocking rule on the settings page, any open tabs that match the rule’ s URL are closed immediately — no need to reload or wait.This makes rule management faster, cleaner, and more intuitive.'
+    'This update improves how rule changes take effect. Now, whenever you add or edit a blocking rule on the settings page, any open tabs that match the rule’ s URL are closed immediately – no need to reload or wait.This makes rule management faster, cleaner, and more intuitive.',
+    'This patch focuses on improving translation quality across the extension. Several localization issues have been corrected to ensure clearer, more accurate messaging for users in all supported languages.'
   ];
   
   const ul = document.getElementById('features');
